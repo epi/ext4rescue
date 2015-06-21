034a198a3f06ad79b39be6b93bdee0c004436970
Adrian Matoga <epi@atari8.info>
2015-06-21 14:38:25 +0200

    Show summary of file system damage

--- main.d
+++ main.d
@@ -55,7 +55,8 @@ void listFiles(FileTree fileTree, Ext4 ext4, ListMode listMode)
 			}
 			else
 			{
-				static class ModeVisitor : FileVisitor {
+				static class ModeVisitor : FileVisitor
+				{
 					char mode;
 					void visit(RegularFile rf) { mode = '-'; }
 					void visit(Directory d) { mode = 'd'; }
@@ -71,11 +72,96 @@ void listFiles(FileTree fileTree, Ext4 ext4, ListMode listMode)
 	}
 }
 
+void showSummary(FileTree fileTree, Ext4 ext4)
+{
+	static struct Summary
+	{
+		uint fileCount;            /// number of files of specific type
+		uint goodInodeCount;       /// number of good inodes
+		uint badInodeCount;        /// number of bad inodes
+		ulong declaredBlockCount;  /// sum of block counts declared in inodes (in 512K blocks)
+		ulong reachableBlockCount; /// sum of block counts reachable via block maps / extent trees
+		ulong readableBlockCount;  /// reachableBlockCount - number of bad blocks reachable via block maps / extent trees
+		ulong badBlockCount;       /// number of blocks that are unreachable or unreadable
+		uint unnamedFileCount;     /// number of files for which names were not found
+	}
+
+	class SummaryVisitor : FileVisitor
+	{
+		Summary regularFileSummary;
+		Summary directorySummary;
+		Summary symbolicLinkSummary;
+
+		void accumulate(SomeFile f, ref Summary s)
+		{
+			if (f.inodeIsOk)
+			{
+				auto inodeStruct = ext4.inodes[f.inodeNum];
+				assert(inodeStruct.ok);
+				++s.goodInodeCount;
+				s.declaredBlockCount += inodeStruct.blockCount >> 1;
+				s.reachableBlockCount += f.mappedByteCount >> 10;
+				s.readableBlockCount += f.readableByteCount >> 10;
+			}
+			else
+			{
+				++s.badInodeCount;
+			}
+			if (f.status.nameUnknown)
+				++s.unnamedFileCount;
+		}
+
+		void visit(Directory d) { accumulate(d, directorySummary); }
+		void visit(RegularFile f) { accumulate(f, regularFileSummary); }
+		void visit(SymbolicLink l) { accumulate(l, symbolicLinkSummary); }
+
+		void postprocess(ref Summary s)
+		{
+			s.fileCount = s.badInodeCount + s.goodInodeCount;
+			s.badBlockCount = s.declaredBlockCount - s.readableBlockCount;
+		}
+
+		void postprocess()
+		{
+			postprocess(regularFileSummary);
+			postprocess(directorySummary);
+			postprocess(symbolicLinkSummary);
+		}
+
+		auto getFieldForAllTypes(string s)() if (__traits(compiles, mixin("Summary.init." ~ s)))
+		{
+			alias FieldType = typeof(mixin("Summary." ~ s ~ ".init"));
+			FieldType[4] result;
+			result[0] = mixin("regularFileSummary." ~ s);
+			result[1] = mixin("directorySummary." ~ s);
+			result[2] = mixin("symbolicLinkSummary." ~ s);
+			result[3] = result[0] + result[1] + result[2];
+			return result;
+		}
+	}
+
+	auto visitor = new SummaryVisitor();
+	foreach (i, sf; fileTree.filesByInodeNum)
+		sf.accept(visitor);
+	visitor.postprocess();
+
+	writefln("%-30s%20s%20s%20s%20s", "file type", "regular file", "directory", "symbolic link", "all");
+	writefln("%-30s%-(%20s%)", "number of files", visitor.getFieldForAllTypes!"fileCount"());
+	writefln("%-30s%-(%20s%)", "good inodes", visitor.getFieldForAllTypes!"goodInodeCount"());
+	writefln("%-30s%-(%20s%)", "bad inodes", visitor.getFieldForAllTypes!"badInodeCount"());
+	writefln("%-30s%-(%20s%)", "files with no name", visitor.getFieldForAllTypes!"unnamedFileCount"());
+	writefln("%-30s%-(%20s%)", "declared 1K-blocks", visitor.getFieldForAllTypes!"declaredBlockCount"());
+	writefln("%-30s%-(%20s%)", "reachable 1K-blocks", visitor.getFieldForAllTypes!"reachableBlockCount"());
+	writefln("%-30s%-(%20s%)", "readable 1K-blocks", visitor.getFieldForAllTypes!"readableBlockCount"());
+	writefln("%-30s%-(%20s%)", "bad or unreachable 1K-blocks", visitor.getFieldForAllTypes!"badBlockCount"());
+}
+
 void main(string[] args)
 {
 	bool forceScan;
+	bool summary;
 	ListMode listMode;
-	getopt(args, "force-scan", &forceScan, "list", &listMode);
+	getopt(args, "force-scan", &forceScan, "list", &listMode, "summary", &summary);
 
 	enforce(args.length >= 2, "Missing ext4 file system image name");
 	string imageName = args[1];
@@ -88,14 +174,16 @@ void main(string[] args)
 	}
 	if (regions.length == 0)
 		regions ~= Region(0, getFileSize(imageName), true);
-	//regions = regions.addRandomDamage(30, 1024);
+	//regions = regions.addRandomDamage(1300, 10240);
 	debug writeln(regions);
 
 	scope ext4 = new Ext4(imageName, regions);
-	writefln("Block size:   %12s", ext4.blockSize);
-	writefln("Inode count:  %12s", ext4.inodes.length);
-	ulong validInodeCount;
-	debug ext4.superBlock.dump();
+	debug
+	{
+		writefln("Block size:   %12s", ext4.blockSize);
+		writefln("Inode count:  %12s", ext4.inodes.length);
+		ext4.superBlock.dump();
+	}
 
 	FileTree fileTree;
 
@@ -129,4 +217,6 @@ void main(string[] args)
 	}
 
 	listFiles(fileTree, ext4, listMode);
+	if (summary)
+		showSummary(fileTree, ext4);
 }
