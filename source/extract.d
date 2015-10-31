2f59b6b3ddf1035a184d46e05b3d71585416497a
Adrian Matoga <epi@atari8.info>
2015-11-01 00:30:49 +0100

    [extract] Added file comment and copyright note.

--- source/extract.d
+++ source/extract.d
@@ -1,3 +1,23 @@
+/**
+	Extract files from the image.
+
+	Copyright:
+	This file is part of ext4rescue $(LINK https://github.com/epi/ext4rescue)
+	Copyright (C) 2015 Adrian Matoga
+
+	ext4rescue is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	ext4rescue is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with ext4rescue.  If not, see $(LINK http://www.gnu.org/licenses/).
+*/
 module extract;
 
 import std.conv;
